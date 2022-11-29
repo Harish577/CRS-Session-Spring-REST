89796c5328dd9d7190b833af0a1190a580a3a277
PIDUGU Harish <harish.pidugu@hexagon.com>
2022-11-29 14:31:01 +0530

    Renamed DAO Impl class

--- src/main/java/com/hexagon/dao/CourseDAOImpl.java
+++ src/main/java/com/hexagon/dao/CourseDAOImpl.java
@@ -6,46 +6,4 @@ import org.springframework.stereotype.Repository;
 public class CourseDAOImpl implements CourseDAOInterface
 {
 
-    private String courseName;
-    private String professor;
-    private String department;
-
-    public String getCourseName()
-    {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName)
-    {
-        this.courseName = courseName;
-    }
-
-    public String getProfessor()
-    {
-        return professor;
-    }
-
-    public void setProfessor(String professor)
-    {
-        this.professor = professor;
-    }
-
-    public String getDepartment()
-    {
-        return department;
-    }
-
-    public void setDepartment(String department)
-    {
-        this.department = department;
-    }
-
-    public CourseDAOImpl(String courseName, String professor, String department)
-    {
-        super();
-        this.courseName = courseName;
-        this.professor = professor;
-        this.department = department;
-
-    }
 }
--- src/main/java/com/hexagon/dao/StudentDAOImpl.java
+++ src/main/java/com/hexagon/dao/StudentDAOImpl.java
@@ -8,58 +8,6 @@ import org.springframework.stereotype.Repository;
 public class StudentDAOImpl implements StudentDAOInterface
 {
 
-    private String              name;
-    private String              college;
-    private Integer             id;
-    private List<CourseDAOImpl> courseList;
-
-    public StudentDAOImpl(String name, String college, Integer id, List<CourseDAOImpl> courseList)
-    {
-        super();
-        this.name = name;
-        this.college = college;
-        this.id = id;
-        this.courseList = courseList;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public String getCollege()
-    {
-        return college;
-    }
-
-    public void setCollege(String college)
-    {
-        this.college = college;
-    }
-
-    public Integer getId()
-    {
-        return id;
-    }
-
-    public void setId(Integer id)
-    {
-        this.id = id;
-    }
-
-    public List<CourseDAOImpl> getCourseList()
-    {
-        return courseList;
-    }
-
-    public void setCourse(List<CourseDAOImpl> courseList)
-    {
-        this.courseList = courseList;
-    }
+    
 
 }
